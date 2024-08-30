a472e0375245f7abd1feb35795370fcde8684887
pedromm90 <pmunem1990@gmail.com>
2024-08-30 14:44:28 -0500

    “Clase1

---
+++ Basic/01-variables.js
@@ -0,0 +1,30 @@
+/*
+Clase 1 en vídeo | 30/08/2024
+Introducción, instalación, hola mundo y variables
+https://www.youtube.com/live/hoULS5isDck?si=qtcStT34Ef8ETBmB&t=915
+*/
+
+// var
+
+var helloWorld = "Hola, JavaScript!";
+console.log(helloWorld);
+
+helloWorld = "¡Hola de nuevo, JavaScript!";
+console.log(helloWorld);
+
+// let
+
+let helloWorld2 = "Hola, JavaScript 2!";
+console.log(helloWorld2);
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!";
+console.log(helloWorld2);
+
+// const
+
+const helloWorld3 = "Hola, JavaScript 3!";
+console.log(helloWorld3);
+
+// Error (Con const, no se puede reasignar una variable)
+// helloWorld3 = "¡Hola de nuevo, JavaScript 3!";
+// console.log(helloWorld3);
